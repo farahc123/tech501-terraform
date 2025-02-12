42a64373bbb143445a026bb93c8931632c5ed362
Farah Cheded <fcheded@gmail.com>
2025-02-12 11:26:50 +0000

    added variables

--- create-ec2-instance/main.tf
+++ create-ec2-instance/main.tf
@@ -1,32 +1,32 @@
 # create an ec2 instance
 
-# where to create it - provide cloud name
+# where to create the EC2 instance - provide cloud provider's name
 
 provider "aws" {
-  # which region to use (where to create these resources)
-  region = "eu-west-1"
+  # which region to use (where to create these resources) -- the below is the same as Ireland
+  region = var.region
 }
 
 # which services/resources with app_instance being my local reference to the resource
 resource "aws_instance" "app_instance" {
 
   # what AMI (amazon machine image) ID to use
-  ami = "ami-0c1c30571d2dae5c9"
+  ami = var.ami_id
 
-  # which type of instance
-  instance_type = "t3.micro"
+  # which type of instance, which specifies CPU cores, memory size, and storage capacity
+  instance_type = var.instance_type
 
   # that we want a public IP
-  associate_public_ip_address = true
+  associate_public_ip_address = var.public_ip_setting
 
-  # Attach the security group defined to the default VPC
+  # attaching the security group defined in security_groups file to the default VPC we have been using
   vpc_security_group_ids = [aws_security_group.public.id]
 
-  # Specify the SSH key to access the instance
-  key_name = "tech501-farah-aws-key"
+  # specifying the SSH key to access the instance; this is already set up on AWS
+  key_name = var.SSH_key_name
 
-  # name the service/instance on AWS
+  # naming the service/instance on AWS via tags
   tags = {
-    Name = "tech501-farah-terraform-app"
+    Name = var.instance_name
   }
 }
--- create-ec2-instance/security_groups.tf
+++ create-ec2-instance/security_groups.tf
@@ -1,29 +1,39 @@
+# creating the security group for the EC2 instance defined in main file
+
+# public is our local reference to this security group; the vpc id (which points to the default VPC we have been using) has been retrieved manually from AWS
+
 resource "aws_security_group" "public" {
-  name        = "tech257-farah-tf-allow-port-22-3000-80"
+  name        = var.security_group_name
   description = "Setup for our first EC2 instance"
-  vpc_id      = "vpc-07e47e9d90d2076da"
+  vpc_id      = var.vpc_id
 
   tags = {
-    Name = "tech257-farah-tf-allow-port-22-3000-80"
+    Name = var.security_group_name
   }
 }
+
+# outbound rule
+# minus 1 protocol means all protocols
+# port 0 is a wildcard telling the system to use whichever port is appropriate for the given protocol (we are allowing all with -1, as above)
 
 resource "aws_security_group_rule" "public_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks = var.all_cidr_blocks
 
   security_group_id = aws_security_group.public.id
 }
 
-resource "aws_security_group_rule" "public_in_ssh_local_host_only" {
+# inbound rules
+
+resource "aws_security_group_rule" "public_in_ssh_all" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = ["127.0.0.1/32"]
+  cidr_blocks       = var.all_cidr_blocks
   security_group_id = aws_security_group.public.id
 }
 
@@ -32,7 +42,7 @@ resource "aws_security_group_rule" "public_in_http_all" {
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = var.all_cidr_blocks
   security_group_id = aws_security_group.public.id
 }
 
@@ -41,6 +51,6 @@ resource "aws_security_group_rule" "public_in_app_all" {
   from_port         = 3000
   to_port           = 3000
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = var.all_cidr_blocks
   security_group_id = aws_security_group.public.id
 }
